adff53870c9f523e3b96ae056170c7931a264788
Tim Lavreniev <tim98lav@gmail.com>
2016-10-06 20:12:39 +0300

    Add files via upload

---
+++ ResolvingEquation.cpp
@@ -0,0 +1,25 @@
+// ResolvingEquation.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Equation is ((x+(3*x)/(x-3))^2)-4+((3*x^2)/(x-3))=\n");
+	int x = 0;
+	float a = 1.1;
+	printf("x = ");
+	scanf("%d", &x);
+	if (x != 3) {
+		a = ((x + (3 * x) / (x - 3)) ^ 2) - 4 + ((3 * x ^ 2) / (x - 3));
+		printf(" Result is %f", a);
+	}
+	else 
+		printf("Ќедопустимое значение");
+	
+	getch();
+
+	return 0;
+}
+
