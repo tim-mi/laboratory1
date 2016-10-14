3c3087e82d2585d398050b76a8d3cbd7257afce7
Tim Lavreniev <tim98lav@gmail.com>
2016-10-14 12:52:17 +0300

    Update ResolvingEquation.cpp

--- ResolvingEquation.cpp
+++ ResolvingEquation.cpp
@@ -1,24 +1,26 @@
-// ResolvingEquation.cpp: îïðåäåëÿåò òî÷êó âõîäà äëÿ êîíñîëüíîãî ïðèëîæåíèÿ.
+// Resolving Equation_.cpp: определяет точку входа для консольного приложения.
 //
 
 #include "stdafx.h"
+#include <math.h>
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-		printf("Equation is x^2+x^3-sqrt(x^2-2x+4)\n");
-		int x;
-		double a, b;
-		printf("x = ");
-		scanf_s("%d", &x);
-		if ((x ^ 2 - 2 * x + 4) >= 0) {
-			b = x*x - 2 * x + 4;
-			a = x*x + x*x*x - sqrt(b);
-			printf(" Result is %f", a);
-		}
-		else
-			printf("ERROR\nInvalid value \n");
-		system("pause");
-		return 0;
+	printf("Equation is x^2+x^3-sqrt(x^2-2x+4)\n");
+	int x;
+	double a,b;
+	printf("x = ");
+	scanf_s("%d", &x);
+	if ((x*x - 2 * x + 4)>=0) {
+		b = x*x - 2 * x + 4;
+		a = x*x  + x*x*x- sqrt(b);
+		printf(" Result is %f", a);
 	}
+	else
+		printf("ERROR\nInvalid value");
 
+	_getch();
+
+	return 0;
+}
