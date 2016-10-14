3f758337e79d63ad009bf1f88b97182f8ff823d6
Tim Lavreniev <tim98lav@gmail.com>
2016-10-14 10:57:35 +0300

    Update ResolvingEquation.cpp

--- ResolvingEquation.cpp
+++ ResolvingEquation.cpp
@@ -6,20 +6,19 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("Equation is ((x+(3*x)/(x-3))^2)-4+((3*x^2)/(x-3))=\n");
-	int x = 0;
-	float a = 1.1;
-	printf("x = ");
-	scanf("%d", &x);
-	if (x != 3) {
-		a = ((x + (3 * x) / (x - 3)) ^ 2) - 4 + ((3 * x ^ 2) / (x - 3));
-		printf(" Result is %f", a);
+		printf("Equation is x^2+x^3-sqrt(x^2-2x+4)\n");
+		int x;
+		double a, b;
+		printf("x = ");
+		scanf_s("%d", &x);
+		if ((x ^ 2 - 2 * x + 4) >= 0) {
+			b = x*x - 2 * x + 4;
+			a = x*x + x*x*x - sqrt(b);
+			printf(" Result is %f", a);
+		}
+		else
+			printf("ERROR\nInvalid value \n");
+		system("pause");
+		return 0;
 	}
-	else 
-		printf("ERROR\nInvalid value");
-	
-	getch();
-
-	return 0;
-}
 
